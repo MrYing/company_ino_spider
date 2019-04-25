68bdf7206e17f354fa9fb47cd843b4e1518654ef
baiping.liu <qdliubp@126.com>
2019-04-25 17:44:59 +0800

    aa

--- company_ifo_spider/pipelines.py
+++ company_ifo_spider/pipelines.py
@@ -13,6 +13,8 @@ class CompanyIfoSpiderPipeline(object):
         self.dict_key = ['企业名称' , '企业描述' , '企业经济性质' , '法人代表或负责人' , '企业类型' , '公司注册地' , '注册资金' , '成立时间' , ' 员工数量' , '月产量' , '年营业额' , '年出口额' , '管理体系认证' , '主要营业地点' , '主要客户' , ' 厂房面积' , '是否提供OEM' , '开户银行' , '银行帐号' , '主要市场' , '主营产品或服务' , '企业网站链接' ]
         if not os.path.exists(self.file_path):#若是存放数据的文件不存在，那么就要写入第一行的表头
             self.file = open(self.file_path,'a',encoding='utf-8' , newline='')
+            import codecs
+            self.file.write(codecs.BOM_UTF8.decode("utf-8"))
             self.w=csv.writer(self.file)
             self.w.writerow(self.dict_key)#写入表头
         else:
--- company_ifo_spider/spiders/run.py
+++ company_ifo_spider/spiders/run.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 from scrapy import cmdline
 
-cmdline.execute('scrapy crawl bafang_spider -a keyword=dashuju'.split())
+cmdline.execute('scrapy crawl bafang_spider -a keyword=rengongzhineng'.split())
